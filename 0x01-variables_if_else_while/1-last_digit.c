8ccd42021243453335a7d463724249faa691fb25
Westmolly <dannydin100.do@gmail.com>
2022-12-10 01:40:03 -0800

    last digit

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -16,7 +16,7 @@ int main(void)
 
 	if (last > 5)
 	{
-		printf("Last digit of %d is %d and is graeter than 5\n", n, last);
+		printf("Last digit of %d is %d and is greater than 5\n", n, last);
 	}
 	else if (last == 0)
 	{
