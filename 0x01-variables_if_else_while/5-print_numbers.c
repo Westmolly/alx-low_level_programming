dd133f53e3c990430c9007a2879a12075353b897
Westmolly <dannydin100.do@gmail.com>
2022-12-11 06:34:53 -0800

    numbers

--- 0x01-variables_if_else_while/5-print_numbers.c
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -3,14 +3,14 @@
 #include <stdlib.h>
 /**
  * main - main block
- * Description: prints all single digit number of base 10
+ * Description: prints ball single digit number of base 10
  * starting from 0, followed by new line.
  * Return: 0
  */
 int main(void)
 {
 	char c = '0';
-	
+
 	while (c <= '9')
 	{
 		putchar(c);
@@ -20,3 +20,4 @@ int main(void)
 	putchar('\n');
 	return (0);
 }
+
