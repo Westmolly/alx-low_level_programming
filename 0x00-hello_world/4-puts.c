fccc42292d373b3b76de7743787e4d364e513f9f
Westmolly <dannydin100.do@gmail.com>
2022-12-12 18:48:31 -0800

    hello puts

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,11 +1,10 @@
 #include <stdio.h>
 /**
- * main - print a line of code using puts
- *
- * Return: 0
+ * main - main block
+ * return: 0
 */
 int main(void)
 {
-	puts("\"programming is like building a multilingual puzzle") :
+	puts("\"programming is like develop a multilingual puzzle");
 		return (0);
 }
