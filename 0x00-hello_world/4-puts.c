93efedf7b1d78ad30c64e22ed540ccf5b33907c0
Westmolly <dannydin100.do@gmail.com>
2022-12-12 18:52:32 -0800

    hello puts

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -2,9 +2,9 @@
 /**
  * main - main block
  * return: 0
-*/
+ */
 int main(void)
 {
-	puts("\"programming is like develop a multilingual puzzle");
+	puts("\"programming is like building a multilingual puzzle");
 		return (0);
 }
