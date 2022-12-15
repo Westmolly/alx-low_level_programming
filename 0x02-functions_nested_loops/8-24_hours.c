aba1886770aaee24b40ed5fdc54f5493a5a4dc0a
Westmolly <dannydin100.do@gmail.com>
2022-12-14 18:24:15 -0800

    8-24_hours.c

--- 0x02-functions_nested_loops/8-24_hours.c
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -1,48 +1,22 @@
 #include "main.h"
 
 /**
- * Jack_bauer - prints every minute of the day of Jack Bauer
- * Return:void
+ * jack_bauer - prints every minute of the day
  */
-
-void Jack_bauer(void)
+void jack_bauer(void)
 {
-
-int h1;
-int h2;
-int m1;
-int m2;
-int a = 9;
-
-h2 = 0;
-while (h2 <= 2)
-{
-	if (h2 == 2)
+	int hour, minute;
+	
+	for (hour = 0; hour <= 23; hour++)
 	{
-		/*Restrain to 23h, not 29*/
-		a = 3;
-	}
-	h1 = 0;
-	while (h1 <= a)
-	{
-		m2 = 0;
-		while (m2 <= 5)
+		for (minute =0; minute <=59; minute++)
 		{
-			m1 = 0;
-			while (m1 <= 9)
-			{
-				_putchar('0' + h2);
-				_putchar('0' + h1);
-				_putchar(':');
-				_putchar('0' + m2);
-				_putchar('0' + m1);
-				_putchar('\n');
-				m1++;
-			}
-			m2++;
+			_putchar((hour / 10) + '0');
+			_putchar((hour % 10) + '0');
+			_putchar(':');
+			_putchar((minute / 10) + '0');
+			_putchar((minute % 10) + '0');
+			_putchar('\n');
 		}
-		h1++;
 	}
-	h2++;
-}
 }
