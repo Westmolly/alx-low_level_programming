e8fc6c9a5a43f36da4939fb486c8d7c932b81bc6
Westmolly <dannydin100.do@gmail.com>
2022-12-14 17:40:48 -0800

    8-24_hours.c

--- 0x02-functions_nested_loops/8-24_hours.c
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -1,11 +1,11 @@
 #include "main.h"
 
 /**
- * jack_bauer - prints every minute of the day of Jack Bauer
+ * Jack_bauer - prints every minute of the day of Jack Bauer
  * Return:void
  */
 
-void jack_bauer(void)
+void Jack_bauer(void)
 {
 
 int h1;
