d7166ee10dfaf2e8b8253519282227e03bc2662f
Westmolly <dannydin100.do@gmail.com>
2022-12-16 17:39:53 -0800

    3-print_numbers.c

---
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * print_numbers - function that print all numbers from 0 - 9
+ * Return: returns void
+ */
+
+void print_numbers(void)
+{
+	int n;
+
+	for (n = 48; n < 58; n++)
+	{
+		putchar(n);
+	}
+	puchar(10);
+}
